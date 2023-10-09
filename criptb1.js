a5540a046fd6218c8563cc6a774b67a48cc969cc
votriet <92041076+votriet@users.noreply.github.com>
2023-10-09 13:44:29 +0700

    Initial commit

---
+++ criptb1.js
@@ -0,0 +1,12 @@
+
+const btn1 = document.getElementById('btn1');
+
+btn1.addEventListener('click', function onClick(event) {
+    // 👇️ change background color
+    document.body.style.backgroundColor = 'black';})
+
+const btn2 = document.getElementById('btn2');
+
+btn2.addEventListener('click', function onClick2(event) {
+    // 👇️ change background color
+    document.body.style.backgroundColor = 'red';})
